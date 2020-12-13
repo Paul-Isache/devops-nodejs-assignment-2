222dc47be82786e15d6566d47e67381bc5ef38e9
Paul Isache <paul@isache.com>
2020-12-13 23:05:18 +0200

    Implement roman translator

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const url = require("url");
 // and returns the number as an integer
 // Don't remember roman numerals? Check https://www.mathsisfun.com/roman-numerals.html
 const romanNumeralTranslator = (str) => {
-  // code goes here
+  const romanNumbers = new Map();
+  romanNumbers.set('M', 1000);
+  romanNumbers.set('D', 500);
+  romanNumbers.set('C', 100);
+  romanNumbers.set('L', 50);
+  romanNumbers.set('X', 10);
+  romanNumbers.set('V', 5);
+  romanNumbers.set('I', 1);
+
+  let currentNumber;
+  let nextNumber;
+
+  return str.toUpperCase().split('').reduce((acc, currentDigit, index, numbers) => {
+    currentNumber = romanNumbers.get(currentDigit);
+    nextNumber = romanNumbers.get(numbers[index + 1]);
+
+    return acc + (((currentNumber >= nextNumber) || (currentNumber && !nextNumber)) ? currentNumber : currentNumber * -1)
+  }, 0);
 };
 
 const handleRequest = (req, res) => {
