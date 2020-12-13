03ab1bfafed6ebabe290fb525d53dfb0de8c4f2c
Paul Isache <paul@isache.com>
2020-12-13 23:12:34 +0200

    Add additional validations on the roman translator and on the request

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const url = require("url");
 // and returns the number as an integer
 // Don't remember roman numerals? Check https://www.mathsisfun.com/roman-numerals.html
 const romanNumeralTranslator = (str) => {
+  if(!str){
+    return 'Invalid number';
+  }
   const romanNumbers = new Map();
   romanNumbers.set('M', 1000);
   romanNumbers.set('D', 500);
@@ -29,9 +32,9 @@ const romanNumeralTranslator = (str) => {
 const handleRequest = (req, res) => {
   const { roman } = url.parse(req.url, true).query;
   res.write(`Hi there! I'm being served from ${os.hostname()} \n\n`);
-  res.write(
-    `The result of ${roman} conversion is ${romanNumeralTranslator(roman)}`
-  );
+
+  res.write(roman ? `The result of ${roman} conversion is ${romanNumeralTranslator(roman)}` : `Please provide a roman number`);
+  
   res.end();
 };
 
--- index.test.js
+++ index.test.js
@@ -3,4 +3,7 @@ const romanNumeralTranslator = require("./index.js");
 test("roman numerals", () => {
   expect(romanNumeralTranslator("MCMLIV")).toBe(1954);
   expect(romanNumeralTranslator("MCCLXXVIII")).toBe(1278);
+
+  expect(romanNumeralTranslator("mcm")).toBe(1900);
+  expect(romanNumeralTranslator("")).toBe('Invalid number');
 });
